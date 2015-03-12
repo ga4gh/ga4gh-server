2648834d8402d2baa0a2e59e6b84a01e2e0f0bb7
Danny Colligan <danny.colligan@gmail.com>
2015-03-12 09:26:12 -0700

    Add datadriven read test data and test skeleton

---
+++ tests/datadriven/test_reads.py
@@ -0,0 +1,29 @@
+"""
+Data-driven tests for reads
+"""
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import ga4gh.protocol as protocol
+import ga4gh.datamodel.reads as reads
+import tests.datadriven as datadriven
+
+
+def testVariantSets():
+    testDataDir = "tests/data/reads"
+    for test in datadriven.makeTests(testDataDir, ReadGroupSetTest):
+        yield test
+
+
+class ReadGroupSetTest(datadriven.DataDrivenTest):
+
+    def getDataModelClass(self):
+        return reads.ReadGroupSet
+
+    def getProtocolClass(self):
+        return protocol.GAReadGroupSet
+
+    def testFixMe(self):
+        readGroupSet = self._gaObject
+        self.assertIsNotNone(readGroupSet)
