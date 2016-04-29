1f027a4a48d830927e3d141e3b4eccdf16e6d78e
David Steinberg <david@resium.com>
2016-04-29 15:30:50 -0700

    Prepare compliance data updated

--- scripts/prepare_compliance_data.py
+++ scripts/prepare_compliance_data.py
@@ -11,13 +11,24 @@ from __future__ import unicode_literals
 import argparse
 import os
 import shutil
-import glob
+import json
 import pysam
 import utils
 import sys
 import generate_gff3_db
 import tempfile
 import zipfile
+
+utils.ga4ghImportGlue()
+
+# We need to turn off QA because of the import glue
+import ga4gh.datarepo as datarepo  # NOQA
+import ga4gh.datamodel.references as references  # NOQA
+import ga4gh.datamodel.datasets as datasets  # NOQA
+import ga4gh.datamodel.variants as variants  # NOQA
+import ga4gh.datamodel.reads as reads  # NOQA
+import ga4gh.datamodel.ontologies as ontologies  # NOQA
+import ga4gh.datamodel.sequenceAnnotations as sequenceAnnotations  # NOQA
 
 
 class ComplianceDataMunger(object):
@@ -33,6 +44,7 @@ class ComplianceDataMunger(object):
         """
         self.inputDirectory = inputDirectory
         self.outputDirectory = outputDirectory
+        self.repoPath = os.path.join(outputDirectory, "repo.db")
         self.tempdir = None
 
         # If no input directory is specified download from GitHub
@@ -49,148 +61,126 @@ class ComplianceDataMunger(object):
                 z.extractall(self.tempdir)
             self.inputDirectory = os.path.join(
                 self.tempdir, 'compliance-master', 'test-data')
-
-        # get all the reference files (they'll be the ones with .fa extension)
-        self.referenceFiles = map(
-            os.path.basename, glob.glob(
-                os.path.join(self.inputDirectory, "*.fa")))
-
-        self.refsetsDirectory = os.path.join(
-            self.outputDirectory, "referenceSets")
-        self.hg37Directory = os.path.join(self.refsetsDirectory, "hg37")
-
-        # datasets
-        self.datasetsDirectory = os.path.join(self.outputDirectory, "datasets")
-
-        readFiles = map(os.path.basename, glob.glob(
-            os.path.join(self.inputDirectory, "*.sam")))
-        variantFiles = map(os.path.basename, glob.glob(
-            os.path.join(self.inputDirectory, "*.vcf")))
-        sequenceAnnotationFiles = map(os.path.basename, glob.glob(
-            os.path.join(self.inputDirectory, "*.gff3")))
-
-        self.datasets = [d for d in set(
-            [p.split('_')[0] for p in (readFiles +
-                                       variantFiles +
-                                       sequenceAnnotationFiles)])]
-        self.datasetDirs = [os.path.join(
-            self.outputDirectory, ds) for ds in self.datasets]
-
-        # Create maps of arrays of files based on dataset.
-        self.datasetReads = dict()
-        self.datasetVariants = dict()
-        self.datasetSequenceAnnotations = dict()
-
-        for ds in self.datasets:
-            self.datasetReads[ds] = [r for r in readFiles if r.startswith(ds)]
-            self.datasetSequenceAnnotations[ds] = [sa for sa in (
-                sequenceAnnotationFiles) if sa.startswith(ds)]
-
-            # Variants themselves are split into groups,
-            # based on second part of the _ split:
-            self.datasetVariants[ds] = dict()
-            # only those variants inside this dataset
-            dsvlist = [v for v in variantFiles if v.startswith(ds)]
-            # create nested dictionary based on group belonging
-            for dsv in dsvlist:
-                dsvGroup = dsv.split('_')[1]
-                self.datasetVariants[ds][dsvGroup] = \
-                    self.datasetVariants[ds].get(dsvGroup, []) + [dsv]
+        repo = datarepo.SqlDataRepository(self.repoPath)
+        self.repo = repo
 
     def run(self):
         if not os.path.exists(self.outputDirectory):
             os.makedirs(self.outputDirectory)
+        self.repo.open("w")
+        self.repo.initialise()
 
-        # Export sequence ontologies
-        print("Exporting ontologies...", file=sys.stderr)
-        ontologiesDir = os.path.join(self.outputDirectory, "ontologymaps")
-        sequenceOntologyDir = os.path.join(ontologiesDir, "sequence_ontology")
-        os.makedirs(sequenceOntologyDir)
-        shutil.copy(os.path.join(self.inputDirectory, "sequence_ontology.txt"),
-                    os.path.join(sequenceOntologyDir, "sequence_ontology.txt"))
+        referenceFileName = "ref_brca1.fa"
+        inputRef = os.path.join(
+            self.inputDirectory, referenceFileName)
+        outputRef = os.path.join(
+            self.outputDirectory, referenceFileName)
+        shutil.copy(inputRef, outputRef)
+        fastaFilePath = os.path.join(
+            self.outputDirectory,
+            referenceFileName + '.gz')
+        pysam.tabix_compress(
+            outputRef, fastaFilePath)
 
-        # Clean out, make and re-populate references directory
-        # For now, assume a single, statically-named referenceSet
-        utils.log("Converting references...")
-        shutil.rmtree(self.refsetsDirectory, ignore_errors=True)
-        os.makedirs(self.refsetsDirectory)
-        shutil.copy(
-            os.path.join(self.inputDirectory, "referenceset_hg37.json"),
-            os.path.join(self.refsetsDirectory, "hg37.json"))
+        with open(
+                os.path.join(
+                    self.inputDirectory, "ref_brca1.json")) as refMetadataFile:
+            refMetadata = json.load(refMetadataFile)
+        with open(
+                os.path.join(
+                    self.inputDirectory,
+                    "referenceset_hg37.json")) as refMetadataFile:
+            refSetMetadata = json.load(refMetadataFile)
 
-        os.makedirs(self.hg37Directory)
-        for refFile in self.referenceFiles:
-            refBase = os.path.splitext(refFile)[0]
-            destFastaFilename = os.path.join(
-                self.hg37Directory, refBase) + ".fa"
-            shutil.copy(os.path.join(self.inputDirectory, refBase) + ".fa",
-                        destFastaFilename)
-            pysam.tabix_compress(destFastaFilename, destFastaFilename + ".gz")
-            refFasta = pysam.FastaFile(destFastaFilename + ".gz")
-            refFasta.close()
-            os.remove(destFastaFilename)
-            shutil.copy(
-                os.path.join(self.inputDirectory, refBase) + ".json",
-                os.path.join(self.hg37Directory, refBase) + ".json")
+        referenceSet = references.HtslibReferenceSet(
+            refSetMetadata['assemblyId'])
 
-        # Clean out, make and repopulate dataset directories
-        shutil.rmtree(self.datasetsDirectory, ignore_errors=True)
-        os.makedirs(self.datasetsDirectory)
+        referenceSet.populateFromFile(fastaFilePath)
+        referenceSet.setAssemblyId(refSetMetadata['assemblyId'])
+        referenceSet.setDescription(refSetMetadata['description'])
+        referenceSet.setNcbiTaxonId(refSetMetadata['ncbiTaxonId'])
+        referenceSet.setIsDerived(refSetMetadata['isDerived'])
+        referenceSet.setSourceUri(refSetMetadata['sourceUri'])
+        referenceSet.setSourceAccessions(refSetMetadata['sourceAccessions'])
+        for reference in referenceSet.getReferences():
+            reference.setNcbiTaxonId(refMetadata['ncbiTaxonId'])
+            reference.setSourceAccessions(
+                refMetadata['sourceAccessions'])
+        self.repo.insertReferenceSet(referenceSet)
 
-        for ds in self.datasets:
-            dsdir = os.path.join(self.datasetsDirectory, ds)
-            os.makedirs(dsdir)
+        dataset = datasets.Dataset("brca1")
+        self.repo.insertDataset(dataset)
 
-            # Reads
-            utils.log("Converting reads...")
-            dsReadsdir = os.path.join(dsdir, "reads")
-            os.makedirs(dsReadsdir)
-            for readFile in self.datasetReads[ds]:
-                destFile = os.path.join(
-                    dsReadsdir,
-                    readFile.split('_')[1].split('.')[0]) + ".bam"
-                readSrc = pysam.AlignmentFile(
-                    os.path.join(self.inputDirectory, readFile), "r")
-                readDest = pysam.AlignmentFile(
-                    destFile, "wb", header=readSrc.header)
-                destFilePath = readDest.filename
+        readFiles = [
+            "brca1_HG00096.sam",
+            "brca1_HG00099.sam",
+            "brca1_HG00101.sam"]
 
-                for readData in readSrc:
-                    readDest.write(readData)
-                readDest.close()
-                readSrc.close()
-                pysam.index(destFilePath)
+        for readFile in readFiles:
+            name = readFile.split('_')[1].split('.')[0]
+            readSrc = pysam.AlignmentFile(
+                os.path.join(self.inputDirectory, readFile), "r")
+            readDest = pysam.AlignmentFile(
+                os.path.join(
+                    self.outputDirectory,
+                    name + ".bam"),
+                "wb", header=readSrc.header)
+            destFilePath = readDest.filename
+            for readData in readSrc:
+                readDest.write(readData)
+            readDest.close()
+            readSrc.close()
+            pysam.index(destFilePath)
+            readGroupSet = reads.HtslibReadGroupSet(dataset, name)
+            readGroupSet.populateFromFile(destFilePath, destFilePath + ".bai")
+            readGroupSet.setReferenceSet(referenceSet)
+            self.repo.insertReadGroupSet(readGroupSet)
 
-            # Variants
-            utils.log("Converting variants...")
-            dsVariantsdir = os.path.join(dsdir, "variants")
-            os.makedirs(dsVariantsdir)
-            for vgroup in self.datasetVariants[ds].keys():
-                vgroupdir = os.path.join(dsVariantsdir, vgroup)
-                os.makedirs(vgroupdir)
-                for variantFile in self.datasetVariants[ds][vgroup]:
-                    destFile = os.path.join(
-                        vgroupdir, variantFile.split('_')[2])
-                    shutil.copy(
-                        os.path.join(
-                            self.inputDirectory, variantFile), destFile)
-                    # Pysam's tabix_index automatically compresses the file
-                    # in place, creates a tabix index.
-                    pysam.tabix_index(destFile, preset="vcf")
+        ontologyMapFileName = "sequence_ontology.txt"
+        inputOntologyMap = os.path.join(
+            self.inputDirectory, ontologyMapFileName)
+        outputOntologyMap = os.path.join(
+            self.outputDirectory, ontologyMapFileName)
+        shutil.copy(inputOntologyMap, outputOntologyMap)
 
-            # Sequence Annotations
-            print("Converting sequence annotations...", file=sys.stderr)
-            dsSeqAnndir = os.path.join(dsdir, "sequenceAnnotations")
-            os.makedirs(dsSeqAnndir)
-            for seqAnnFile in self.datasetSequenceAnnotations[ds]:
-                seqAnnDest = os.path.join(
-                    dsSeqAnndir,
-                    seqAnnFile.split('_')[1].split('.')[0]) + ".db"
-                seqAnnSrc = os.path.join(self.inputDirectory, seqAnnFile)
-                dbgen = generate_gff3_db.Gff32Db(seqAnnSrc, seqAnnDest)
-                dbgen.run()
+        sequenceOntology = ontologies.OntologyTermMap("sequence_ontology")
+        sequenceOntology.populateFromFile(outputOntologyMap)
+        self.repo.insertOntologyTermMap(sequenceOntology)
+        self.repo.addOntologyTermMap(sequenceOntology)
 
-        print("done converting compliance data.", file=sys.stderr)
+        map(lambda x: self.addVariantSet(x, dataset, referenceSet),
+            ["brca1_1kgPhase3_variants.vcf",
+             "brca1_WASH7P_annotation.vcf",
+             "brca1_OR4F_annotation.vcf"])
+
+        seqAnnFile = "brca1_gencodev19.gff3"
+        seqAnnSrc = os.path.join(self.inputDirectory, seqAnnFile)
+        seqAnnDest = os.path.join(self.outputDirectory, "gencodev19.db")
+        dbgen = generate_gff3_db.Gff32Db(seqAnnSrc, seqAnnDest)
+        dbgen.run()
+        gencode = sequenceAnnotations.Gff3DbFeatureSet(dataset, "gencodev19")
+        gencode.populateFromFile(seqAnnDest)
+        gencode.setReferenceSet(referenceSet)
+        self.repo.insertFeatureSet(gencode)
+
+        self.repo.commit()
+
+        print("Done converting compliance data.", file=sys.stderr)
+
+    def addVariantSet(self, variantFileName, dataset, referenceSet):
+        inputVcf = os.path.join(
+            self.inputDirectory, variantFileName)
+        outputVcf = os.path.join(
+            self.outputDirectory, variantFileName)
+        shutil.copy(inputVcf, outputVcf)
+        pysam.tabix_index(outputVcf, preset="vcf")
+        variantSet = variants.HtslibVariantSet(
+            dataset, variantFileName.split('_')[1])
+        variantSet.setReferenceSet(referenceSet)
+        variantSet.populateFromFile(
+            [outputVcf + ".gz"], [outputVcf + ".gz.tbi"])
+        variantSet.checkConsistency()
+        self.repo.insertVariantSet(variantSet)
 
     def cleanup(self):
         if self.tempdir is not None:
