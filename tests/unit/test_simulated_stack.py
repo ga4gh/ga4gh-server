5b4f85f083ee0515355d31bfcd95e3f7e2480190
David Steinberg <david@resium.com>
2016-07-20 13:40:08 -0700

    Fix VA search bug During the protobuf conversion extend was incorrectly used. Fixing this bug revealed that the simulated stack tests for VA were incorrect and generating transcript effect records for the same alternate base. This PR removes the uncecessary outer loop.

--- tests/unit/test_simulated_stack.py
+++ tests/unit/test_simulated_stack.py
@@ -722,6 +722,9 @@ class TestSimulatedStack(unittest.TestCase):
                            "There should be some results for a good effect ID")
         for ann in responseData.variant_annotations:
             effectPresent = False
+            txIds = map(lambda t: t.id, ann.transcript_effects)
+            self.assertEqual(len(txIds), len(set(txIds)),
+                             "Transcript effects should be unique")
             for effect in ann.transcript_effects:
                 for featureType in effect.effects:
                     if featureType.id in map(
