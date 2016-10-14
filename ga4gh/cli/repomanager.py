15538b8a0d2190d07e28c22a3115fed3012eec14
Danny Colligan <danny.colligan@gmail.com>
2016-10-14 13:01:17 -0400

    Fix remove-rnaquantificationset command
    Issue #1423
    
    Same changes as ga4gh/server#1424
    Real author is @kozbo

--- ga4gh/cli/repomanager.py
+++ ga4gh/cli/repomanager.py
@@ -544,7 +544,8 @@ class RepoManager(object):
             self._args.rnaQuantificationSetName)
 
         def func():
-            self._updateRepo(self._repo.removeRnaQuantification, rnaQuantSet)
+            self._updateRepo(self._repo.removeRnaQuantificationSet,
+                             rnaQuantSet)
         self._confirmDelete(
             "RnaQuantificationSet", rnaQuantSet.getLocalId(), func)
 
@@ -998,7 +999,8 @@ class RepoManager(object):
             runner="removeRnaQuantificationSet")
         cls.addRepoArgument(removeRnaQuantificationSetParser)
         cls.addDatasetNameArgument(removeRnaQuantificationSetParser)
-        cls.addRnaQuantificationNameArgument(removeRnaQuantificationSetParser)
+        cls.addRnaQuantificationSetNameArgument(
+            removeRnaQuantificationSetParser)
         cls.addForceOption(removeRnaQuantificationSetParser)
 
         addPhenotypeAssociationSetParser = cli.addSubparser(
