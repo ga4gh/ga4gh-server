22fc2d903964fbfa9bbaa837bf8c4b5792e67a5d
Andrew Jesaitis <andrew@andrewjesaitis.com>
2017-01-11 17:29:27 -0700

    Fixup demo_example script (#1522)
    Updated client import path and attribute names.

--- scripts/demo_example.py
+++ scripts/demo_example.py
@@ -6,19 +6,19 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import ga4gh.common.client as client
+import ga4gh.client.client as client
 
 
 def runDemo():
 
     httpClient = client.HttpClient("http://localhost:8000")
     iterator = httpClient.search_variants(
-        variantSetId="MWtnLXAzLXN1YnNldDptdm5jYWxs",
-        referenceName="1", start=45000, end=50000)
+        "WyIxa2ctcDMtc3Vic2V0IiwidnMiLCJtdm5jYWxsIl0",
+        reference_name="1", start=45000, end=50000)
     for variant in iterator:
         print(
-            variant.referenceName, variant.start, variant.end,
-            variant.referenceBases, variant.alternateBases, sep="\t")
+            variant.reference_name, variant.start, variant.end,
+            variant.reference_bases, variant.alternate_bases, sep="\t")
 
 
 if __name__ == '__main__':
